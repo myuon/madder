5761a0be208e993fb1a4ab279e91fcd64fdab962
myuon <ioi.joi.koi.loi@gmail.com>
2018-02-15 17:06:57 +0900

    Overlay event passthough NB: clicking event on component widget will also emit the signal to fixed_box too

--- src/widget/timeline.rs
+++ src/widget/timeline.rs
@@ -17,45 +17,53 @@ use widget::RulerWidget;
 
 pub struct TimelineWidget {
     fixed: gtk::Fixed,
+    fixed_box: gtk::EventBox,
     tracker: gtk::DrawingArea,
-    container: gtk::EventBox,
+    container: gtk::Overlay,
     offset: i32,
 }
 
 // workaround for sharing a variable within callbacks
 impl TimelineWidget {
     pub fn new(width: i32) -> Rc<RefCell<TimelineWidget>> {
-        let evbox = gtk::EventBox::new();
-
+        let fixed_box = gtk::EventBox::new();
         let fixed = gtk::Fixed::new();
         fixed.set_size_request(width, 50);
 
-        let overlay = gtk::Overlay::new();
-        evbox.add(&overlay);
+        fixed_box.add(&fixed);
 
+        let overlay = gtk::Overlay::new();
         {
             let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
             overlay.add(&vbox);
 
             let ruler = RulerWidget::new(width);
             vbox.pack_start(ruler.to_widget(), true, true, 10);
-            vbox.pack_start(&fixed, true, true, 0);
+
+            vbox.pack_start(&fixed_box, true, true, 0);
         }
 
         let tracker = gtk::DrawingArea::new();
         overlay.add_overlay(&tracker);
         tracker.set_size_request(width, 50);
 
+        overlay.set_overlay_pass_through(&tracker, true);
+        tracker.connect_realize(move |tracker| {
+            let window = tracker.get_window().unwrap();
+            window.set_pass_through(true);
+        });
+
         Rc::new(RefCell::new(TimelineWidget {
             fixed: fixed,
-            container: evbox,
+            fixed_box: fixed_box,
+            container: overlay,
             tracker: tracker,
             offset: 0
         }))
     }
 
     pub fn connect_button_press_event<F: Fn(&gdk::EventButton) -> gtk::Inhibit + 'static>(&self, cont: F) {
-        self.container.connect_button_press_event(move |_, event| {
+        self.fixed_box.connect_button_press_event(move |_, event| {
             cont(event)
         });
     }
@@ -73,6 +81,10 @@ impl TimelineWidget {
     pub fn add_component_widget(self_: Rc<RefCell<TimelineWidget>>, label_text: &str, offset_x: i32, width: i32) {
         let evbox = gtk::EventBox::new();
         evbox.show();
+        evbox.connect_realize(move |evbox| {
+            let window = evbox.get_window().unwrap();
+            window.set_pass_through(false);
+        });
 
         {
             let builder: &RefCell<TimelineWidget> = self_.borrow();
@@ -130,7 +142,7 @@ impl TimelineWidget {
 }
 
 impl WidgetWrapper for TimelineWidget {
-    type T = gtk::EventBox;
+    type T = gtk::Overlay;
 
     fn to_widget(&self) -> &Self::T {
         &self.container
