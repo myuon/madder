34932578483b716c0f83414be14846836e71f8b7
myuon <ioi.joi.koi.loi@gmail.com>
2018-09-29 19:50:38 +0900

    feat: audio stream with audiotestsrc

--- core/src/spec/adapter/avi_renderer.rs
+++ core/src/spec/adapter/avi_renderer.rs
@@ -20,23 +20,24 @@ pub struct AviRenderer {
 }
 
 impl AviRenderer {
-    pub fn new(uri: &str, audio_pipelines: Vec<Rc<gst::Pipeline>>, width: i32, height: i32, frames: i32, delta: u64) -> AviRenderer {
-        let pipeline = gst::Pipeline::new(None);
-        let appsrc = gst::ElementFactory::make("appsrc", None).unwrap();
-        let videoconvert = gst::ElementFactory::make("videoconvert", None).unwrap();
-
+    pub fn new(uri: &str, audio_streams: Vec<(gst::ClockTime, Vec<gst::Element>)>, width: i32, height: i32, frames: i32, delta: u64) -> AviRenderer {
         let audiotestsrc = gst::ElementFactory::make("audiotestsrc", None).unwrap();
         let audiomix = gst::ElementFactory::make("audiomixer", None).unwrap();
         let audioconvert = gst::ElementFactory::make("audioconvert", None).unwrap();
         let audiorate = gst::ElementFactory::make("audiorate", None).unwrap();
 
+        let pipeline = gst::Pipeline::new(None);
+        let appsrc = gst::ElementFactory::make("appsrc", None).unwrap();
+        let videoconvert = gst::ElementFactory::make("videoconvert", None).unwrap();
+
         let avimux = gst::ElementFactory::make("avimux", None).unwrap();
+        let h264enc = gst::ElementFactory::make("x264enc", None).unwrap();
         let sink = gst::ElementFactory::make("filesink", None).unwrap();
         sink.set_property("location", &uri).unwrap();
 
-        pipeline.add_many(&[&appsrc, &videoconvert, &audiotestsrc, &audiomix, &avimux, &audioconvert, &audiorate, &sink]).unwrap();
+        pipeline.add_many(&[&appsrc, &videoconvert, &audiotestsrc, &audiomix, &avimux, &audioconvert, &audiorate, &h264enc, &sink]).unwrap();
         gst::Element::link_many(&[&audiotestsrc, &audiomix, &audioconvert, &audiorate, &avimux]).unwrap();
-        gst::Element::link_many(&[&appsrc, &videoconvert, &avimux, &sink]).unwrap();
+        gst::Element::link_many(&[&appsrc, &videoconvert, &h264enc, &avimux, &sink]).unwrap();
 
         let appsrc = appsrc.dynamic_cast::<gsta::AppSrc>().unwrap();
         let info = gstv::VideoInfo::new(gstv::VideoFormat::Rgb, width as u32, height as u32).fps(gst::Fraction::new(20,1)).build().unwrap();
@@ -111,8 +112,7 @@ pub trait HaveAviRenderer : HavePresenter {
 
     fn render_new(&mut self, uri: &str, frames: i32, delta: u64) -> AviRenderer {
         let size = self.project().size;
-        let audio_pipelines = self.get_audio_pipelines();
-        AviRenderer::new(uri, audio_pipelines, size.0, size.1, frames, delta)
+        AviRenderer::new(uri, self.get_audio_streams(), size.0, size.1, frames, delta)
     }
 
     fn render_init(&mut self, uri: &str, frames: i32, delta: u64);
